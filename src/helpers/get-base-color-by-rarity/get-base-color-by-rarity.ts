86b06a75cc9fcff893f37cb852a9915c4c5c7689
kirilinsky <kirill.ilinsky@gmail.com>
2025-05-13 11:21:05 +0200

    update color scale

--- src/helpers/get-base-color-by-rarity/get-base-color-by-rarity.ts
+++ src/helpers/get-base-color-by-rarity/get-base-color-by-rarity.ts
@@ -1,7 +1,7 @@
 export function getBaseColorByScale(scale: string): string {
   switch (scale.toLowerCase()) {
     case "common":
-      return "white";
+      return "gray";
     case "rare":
       return "blue";
     case "epic":
@@ -11,8 +11,10 @@ export function getBaseColorByScale(scale: string): string {
     case "divine":
       return "gold";
     case "ghost":
+      return "white";
+    case "mystic":
       return "green";
     default:
-      return "gray";
+      return "black";
   }
 }
