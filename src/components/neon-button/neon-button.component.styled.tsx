3a276eea39206776b4c761763845cca52c64a028
kirilinsky <kirill.ilinsky@gmail.com>
2025-05-20 11:45:38 +0200

    update button

--- src/components/neon-button/neon-button.component.styled.tsx
+++ src/components/neon-button/neon-button.component.styled.tsx
@@ -1,22 +1,40 @@
 import styled from "styled-components";
 
 export const NeonButtonStyled = styled.button`
-  padding: 10px 15px;
-  min-width: 150px;
-  max-width: max-content;
-  font-family: "Lato", sans-serif;
-  font-weight: 500;
-  background: transparent;
-  cursor: pointer;
-  transition: all 0.3s ease;
   position: relative;
-  display: inline-block;
-  color: #0bf4f3;
-  border: 1px solid #0bf4f3;
-
-  line-height: 20px;
-  &:hover {
-    box-shadow: 0 0 10px #28eded, 0 0 9px #38fbfb inset;
-    border: 1px solid #97f9f9;
+  z-index: 1;
+  background: #1c031a;
+  border: 3px solid;
+  border-color: #b61aa6;
+  border-radius: 0.75rem;
+  color: #cd20bb;
+  padding: 0.75rem 1rem;
+  text-decoration: none;
+  transition: 250ms ease-in-out;
+  transition-property: all;
+  & span {
+    filter: drop-shadow(0 0 1px currentcolor);
+  }
+  &:after {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    inset: 0;
+    opacity: 0.6;
+    border-radius: inherit;
+    box-shadow: 0 0 1em 0.4em #cd20bb;
+    transition: 250ms ease-in-out;
+    transition-property: opacity;
+  }
+  &:hover,
+  &:focus {
+    color: #ae3da3;;
+    background-color: #290626;;
+    transform: scale(1.01);
+    cursor: pointer;
+  }
+  &:hover::after,
+  &:focus:after {
+    opacity: 0.6;
   }
 `;
