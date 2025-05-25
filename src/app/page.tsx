e7a52c7ec58ff8db2658466d44e45cd1c6dd7948
kirilinsky <kirill.ilinsky@gmail.com>
2025-05-25 17:56:47 +0200

    update roadmap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,6 +44,7 @@ export default function Home() {
           <PageContentBlockStyled>
             <h2>Road map</h2>
             <ul>
+              <li>I18n</li>
               <li>Pay to skip function</li>
               <li>Funds system</li>
               <li>Anonymous home page</li>
@@ -56,7 +57,6 @@ export default function Home() {
               <li>Announcements</li>
               <li>Tutorial page</li>
               <li>SignUp/Login via gmail</li>
-              <li>I18n</li>
             </ul>
           </PageContentBlockStyled>
           <PageContentBlockStyled>
