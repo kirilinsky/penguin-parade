b80310740c7f76c5c5cc83c51f83769dd1991a2b
kirilinsky <kirill.ilinsky@gmail.com>
2025-05-27 13:51:31 +0200

    update friends page

--- src/components/friends-list-block/friends-list-block.component.styled.tsx
+++ src/components/friends-list-block/friends-list-block.component.styled.tsx
@@ -4,13 +4,13 @@ export const FriendsListBlockGrid = styled.div`
   display: grid;
   padding: 10px;
   width: 100%;
-  max-height: 40vh;
+  max-height: 50vh;
   gap: 15px;
   grid-template-columns: repeat(2, 1fr);
   overflow-y: auto;
   @media (max-width: 768px) {
     grid-template-columns: 1fr;
-    max-height:50vh;
+
     padding: 10px;
   }
 `;
