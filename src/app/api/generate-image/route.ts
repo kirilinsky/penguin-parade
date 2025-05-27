6ebefc0ea72eb92b7be4f631e579b81db58def07
kirilinsky <kirill.ilinsky@gmail.com>
2025-05-27 12:01:15 +0200

    fix generator

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -178,7 +178,7 @@ export async function POST(req: Request) {
       collection(firestore, GLOBAL_IMAGES_COLLECTION),
       {
         imageUrl: urlData.publicUrl,
-        title: settings.t || "Untitled",
+        title: settings.en.t || "Untitled",
         creatorUid: uid,
         ownerId: uid,
         origin,
